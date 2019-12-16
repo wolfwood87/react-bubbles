31509d2779d29e2a5a14e975da80f39e3536d37a
Derek <legato_5@yahoo.com>
2019-12-15 18:32:02 -0800

    token now saves to storage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,28 +3,52 @@ import React, { useState } from "react";
 import axios from 'axios';
 
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   const [cred, setCred] = useState({username: '', password: ''})
-  const [load, setLoad] = useState(false);
+  // const [load, setLoad] = useState(false);
 
+  const handleChange = e => {
+    e.preventDefault();
+    setCred({
+      ...cred,
+      [e.target.name]: e.target.value
+    })
+  };
 
+  const login = e => {
+    e.preventDefault();
+    setTimeout(() => {console.log('Loading');
+    axios
+        .post('http://localhost:5000/api/login', cred)
+        .then(res => {
+          console.log('bk: Login.js: Login: login: res: ', res)
+          localStorage.setItem('token', res.data.payload)
+          props.history.push('/bubble-page')
+        })
+        .catch(err => {console.log(err.message)})
+  })
+  }
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <form>
+      <form onSubmit={login} className="login-form">
           <input
             type='text'
             name='username'
             placeholder='Username'
+            value={cred.username}
+            onChange={handleChange}
           />
           <input
             type='password'
             name='password'
             placeholder='Password'
+            value={cred.password}
+            onChange={handleChange}
             />
-            <button>Log In</button>
+            <button className="login-button">Log In</button>
       </form>
     </>
   );
