250876625e771c45f7cf720e75a947ab739e4ecf
Derek <legato_5@yahoo.com>
2019-12-15 18:18:40 -0800

    login page created.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+import axios from 'axios';
+
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [cred, setCred] = useState({username: '', password: ''})
+  const [load, setLoad] = useState(false);
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form>
+          <input
+            type='text'
+            name='username'
+            placeholder='Username'
+          />
+          <input
+            type='password'
+            name='password'
+            placeholder='Password'
+            />
+            <button>Log In</button>
+      </form>
     </>
   );
 };
